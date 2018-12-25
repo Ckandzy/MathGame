af52de94e67ca170695a030e1fb6beef230ed125
Hotkang <353223003@qq.com>
2018-12-25 15:04:35 +0800

    Repository initialization

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public enum EnumMoveMode
+    {
+        StraightLine = 0,
+        InnerCycloid,
+        OuterCycloid,
+    }
+    public EnumMoveMode moveMode = EnumMoveMode.StraightLine;
+    public float moveSpeed = 1f;
+    public Transform center;
+
+    public float lastDegree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lastDegree = Mathf.Acos((transform.position - center.position).x / (transform.position - center.position).magnitude);
+        lastDegree = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float speed = moveSpeed * Time.deltaTime;
+        float radius = GetComponent<CircleCollider2D>().radius * transform.localScale.x;
+        switch (moveMode)
+        {
+            case EnumMoveMode.StraightLine:
+                {
+                    transform.localPosition += new Vector3(speed, 0, 0);
+                    // degree per second = 360 / (2*PI*r/speed)
+                    transform.Rotate(0, 0, -(360 / (2 * Mathf.PI * radius / moveSpeed)) * Time.deltaTime, Space.Self);
+                }break;
+            case EnumMoveMode.InnerCycloid:
+                {
+                    float rCenter = (center.position - transform.position).magnitude;
+                    float rEdge = rCenter + radius;
+                    float perimeterEdge = 2 * rEdge * Mathf.PI;
+                    //Debug.Log(rCenter + "," + rEdge + "," + perimeterEdge + "," + 2 * rEdge * radius);
+                    transform.localPosition = new Vector3(Mathf.Cos(Mathf.Deg2Rad * (lastDegree + 360 / (perimeterEdge / speed))) * rCenter,
+                        Mathf.Sin(Mathf.Deg2Rad * (lastDegree + 360 / (perimeterEdge / speed))) * rCenter,
+                        0);
+                    lastDegree = (lastDegree + 360 / (perimeterEdge / speed)) % 360;
+
+                    transform.Rotate(0, 0, -(360 / (perimeterEdge / speed) * rCenter / rEdge), Space.Self);
+                }break;
+            case EnumMoveMode.OuterCycloid:
+                {
+
+                }break;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float gravity;
+    public float jumpSpeed;
+    public Vector2 moveVerctor;
+    public bool isOriginRight;
+    public bool isGrounded;
+    public float raycastDistance;
+    public LayerMask groundedLayerMask;
+
+    protected readonly int m_HashHorizontalSpeedPara = Animator.StringToHash("HorizontalSpeed");
+    protected readonly int m_HashGroundedPara = Animator.StringToHash("Grounded");
+    protected Animator m_Animator;
+    protected CapsuleCollider2D m_Capsule;
+    protected Rigidbody2D m_Rigidbody2D;
+    public SpriteRenderer spriteRenderer;
+
+    Vector2 m_PreviousPosition;
+    Vector2 m_CurrentPosition;
+    Vector2 m_NextMovement;
+    ContactFilter2D contactFilter;
+
+    public PlayerInput playerInput;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Capsule = GetComponent<CapsuleCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+        contactFilter.layerMask = groundedLayerMask;
+        contactFilter.useLayerMask = true;
+        contactFilter.useTriggers = false;
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateFacing();
+        moveVerctor.x = PlayerInput.Instance.Horizontal.Value * moveSpeed;
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            moveVerctor += new Vector2(0, jumpSpeed);
+        }
+        CheckCapsuleEndCollisions(true);
+        m_Animator.SetFloat(m_HashHorizontalSpeedPara, moveVerctor.x);
+        m_Animator.SetBool(m_HashGroundedPara, isGrounded);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isGrounded)
+        {
+            moveVerctor += new Vector2(0, gravity);
+        }
+        else if (moveVerctor.y < 0)
+        {
+            moveVerctor.y = 0;
+        }
+        m_NextMovement = moveVerctor * Time.deltaTime;
+        m_PreviousPosition = m_Rigidbody2D.position;
+        m_CurrentPosition = m_PreviousPosition + m_NextMovement;
+        m_NextMovement = Vector2.zero;
+        m_Rigidbody2D.MovePosition(m_CurrentPosition);
+    }
+
+    public void CheckCapsuleEndCollisions(bool bottom = true)
+    {
+        Vector2 raycastDirection = Vector2.down;
+        Vector2 raycastStart;
+        raycastStart = m_Rigidbody2D.position;
+        RaycastHit2D[] raycastHit = new RaycastHit2D[12];
+        if (bottom)
+        {
+            Debug.DrawRay(raycastStart, raycastDirection);
+            if (Physics2D.Raycast(raycastStart, raycastDirection, contactFilter, raycastHit, raycastDistance) > 0)
+            {
+                isGrounded = true;
+            }
+            else
+            {
+                isGrounded = false;
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    public void UpdateFacing()
+    {
+        bool faceLeft = PlayerInput.Instance.Horizontal.Value < 0f;
+        bool faceRight = PlayerInput.Instance.Horizontal.Value > 0f;
+
+        if (faceLeft)
+        {
+            spriteRenderer.flipX = isOriginRight;
+        }
+        else if (faceRight)
+        {
+            spriteRenderer.flipX = !isOriginRight;
+        }
+    }
+}
